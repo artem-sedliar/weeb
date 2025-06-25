6001570b70664c80df5056fea8a4d3ec0df7727c
artem-sedliar <artemsedlar@gmail.com>
2025-06-25 12:20:19 +0300

    completed

---
+++ js/script.js
@@ -0,0 +1,32 @@
+"use strict"
+
+let documentActions = (e) => {
+	const targetElement = e.target
+	if (targetElement.closest('.icon-menu')) {
+		document.documentElement.classList.toggle('open-menu')
+	}
+}
+document.addEventListener('click', documentActions)
+
+
+const options = {
+	root: null,
+	rootMargin: "0px 0px 0px 0px",
+	threshold: 0.2,
+}
+const callback = (entries, observer) => {
+	entries.forEach(entry => {
+		const currentElement = entry.target
+		if (entry.isIntersecting) {
+			currentElement.classList.add('--animate')
+		} else {
+			currentElement.classList.remove('--animate')
+		}
+	})
+}
+const observer = new IntersectionObserver(callback, options)
+
+const animElements = document.querySelectorAll('[class*="--anim"]')
+animElements.forEach(animElement => {
+	observer.observe(animElement)
+})
